4baec5e49ba1455fadf07fcf6c85ac834cd2708b
lembak51 <lembak51@gmail.com>
2024-03-25 11:45:25 +0200

    Add files via upload

---
+++ finance4_first_try/src/test/java/stepdefinitions/APIRequest.java
@@ -0,0 +1,34 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static config.Config.baseUrl;
+import static config.Config.endPoint;
+import static io.restassured.RestAssured.given;
+
+public class APIRequest {
+public static String authorFromApi = "test" ;
+    @And("get author from API")
+    public void   getRequestWithQueryParam() {
+    RestAssured.baseURI = baseUrl;
+        Response response = given()
+                .when()
+                .get(endPoint)
+                .then()
+                .extract().response();
+       assertThat(200, equalTo(response.statusCode()));
+
+        String htmlContent = response.getBody().asString();
+        // Parse the HTML content using Jsoup
+        Document doc = Jsoup.parse(htmlContent);
+        authorFromApi = doc.select("a[itemprop='author']").first().text();
+    }
+}
